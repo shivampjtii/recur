c63364b4fe27b19ee6ca19b15a53e6095c71268a
shivampjtii <shivampjtii@gmail.com>
2026-03-30 19:27:27 +0530

    cleaned

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import {styled} from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
